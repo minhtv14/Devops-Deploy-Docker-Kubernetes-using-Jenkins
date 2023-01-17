9073e04f4310b17d9c57a80cc84cde75c04d570a
101389902+minhtv1452002@users.noreply.github.com <101389902+minhtv1452002@users.noreply.github.com>
2023-01-18 05:09:56 +0700

    [MinhTv] Update pom.xml

--- src/main/java/com/devops/vn/DevopsServer.java
+++ src/main/java/com/devops/vn/DevopsServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmintoServerApplication {
+public class DevopsServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmintoServerApplication.class, args);
+		SpringApplication.run(DevopsServer.class, args);
 	}
 
 }
